81a7781197921dc0ae8c193dd4d72531cb83170e
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-03-18 16:21:05 +0100

    add favorite counter

--- src/components/FavoritesDropdown.jsx
+++ src/components/FavoritesDropdown.jsx
@@ -18,10 +18,10 @@ export const FavoritesDropdown = () => {
             <div className="dropdown">
                 <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Favorites
+                    <span className="badge text-bg-secondary ms-2">{store.favs.length}</span>
                 </button>
                 <ul className="dropdown-menu dropdown-menu-end text-end border-0 border-end rounded-0" style={{ width: 300 }}>
                     {store.favs.length !== 0 ? store.favs.map((element, i) => {
-                        console.log(element)
                         return <li key={i} className="me-2">
                                     {element}
                                     <RemoveFavoriteButton onSelectedButton={() => removeFavorite(element)} />
